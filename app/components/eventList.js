0bdaf9f8ec3c5fbf11556fe927a14b68783fb40b
Anant Batgali <anantbatgali@Anants-Air.attlocal.net>
2023-09-21 14:26:53 -0400

    Events layout

--- app/components/eventList.js
+++ app/components/eventList.js
@@ -1,7 +1,31 @@
 export default function Eventlist({ events }) {
+  
   return (
     <>
-    {console.log(events)}
+    {/* {console.log(events)} */}
+    
+    <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6 my-5">
+      <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+        <div className="ml-4 mt-2">
+          <h3 className="text-base font-semibold leading-6 text-gray-900">Event Name</h3>
+        </div>
+        <div className="ml-4 mt-2 flex-shrink-0">
+          <button
+            type="button"
+            className="relative inline-flex items-center rounded-md bg-amber-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            View Details
+          </button>
+        </div>
+      </div>
+      <div className="p-2">
+        <span>Date </span>
+        &nbsp; 
+        <span>Time</span>
+
+      </div>
+    </div>
+
     </>
   )
 }
---
+++ app/events/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+export default function EventsLayout({children}){
+  return (
+    
+   
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+      <div className="mx-auto max-w-3xl">{ children }</div>
+    </div>
+
+  )
+}
