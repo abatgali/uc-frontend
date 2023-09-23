08fe7c1320761f7f1534f34b97dec393379c12e3
Anant Batgali <anantbatgali@Anants-Air.attlocal.net>
2023-09-23 17:00:58 -0400

    font weight logo change

--- app/components/logo.js
+++ app/components/logo.js
@@ -3,7 +3,7 @@ import { playfair } from "../pages/_app"
 export default function Logo() {
   return (
     
-    <p className={` font-bold ${playfair.className} text-xl md:text-2xl text-amber-500`}>United Chin International</p>
+    <p className={` ${playfair.className} text-xl md:text-2xl font-semibold text-amber-500`}>United Chin International</p>
 
   )
 }
