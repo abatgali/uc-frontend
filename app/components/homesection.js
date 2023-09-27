033eaadf27e8441a9dfdb54c3c92c0c075c93767
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-09-27 13:47:27 -0400

    revised fix

--- app/components/homesection.js
+++ app/components/homesection.js
@@ -1,145 +1,150 @@
-'use client'
+"use client";
 
 export default function SectionHome() {
   return (
     <>
-  
-    <div className='bg-white'>
-    <div className="w-full mb-10 h-[350px] bg-black text-center text-cyan-50 relative overflow-hidden">
-          <img src="https://res.cloudinary.com/dtyw96fds/image/upload/v1695832312/bryan-plata-LUIYexdkmgE-unsplash_fqzhpl.jpg" className="w-full object-cover h-[320px] absolute top-0 left-0">
-          </img>
-          <h2 className="relative font-semibold top-1/3 w-2/3 mx-auto z-10">UC Cup isn't just about activities; it's a hub for building enduring friendships and meaningful connections. Meet kindred spirits who share your passions, aspirations, and dreams. Together, you'll craft unforgettable memories that will stand the test of time.</h2>
-    </div> 
-      <main>
-        
-        
-        <div className=' isolate'>
-          <svg
-            className='absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]'
-            aria-hidden='true'
-          >
-            <defs>
-              <pattern
-                id='1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84'
-                width={200}
-                height={200}
-                x='50%'
-                y={-1}
-                patternUnits='userSpaceOnUse'
-              >
-                <path d='M.5 200V.5H200' fill='none' />
-              </pattern>
-            </defs>
-            <svg x='50%' y={-1} className='overflow-visible fill-gray-50'>
-              <path
-                d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
+      <div className="bg-white">
+        <div className="w-full mb-10 h-[350px] bg-black text-center text-cyan-50 relative overflow-hidden">
+          <img
+            src="https://res.cloudinary.com/dtyw96fds/image/upload/v1695832312/bryan-plata-LUIYexdkmgE-unsplash_fqzhpl.jpg"
+            className="w-full object-cover h-[320px] absolute top-0 left-0"
+          ></img>
+          <h2 className="relative font-semibold top-1/3 w-2/3 mx-auto z-10">
+            UC Cup isn't just about activities; it's a hub for building enduring
+            friendships and meaningful connections. Meet kindred spirits who
+            share your passions, aspirations, and dreams. Together, you'll craft
+            unforgettable memories that will stand the test of time.
+          </h2>
+        </div>
+        <main>
+          <div className=" isolate">
+            <svg
+              className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+                  width={200}
+                  height={200}
+                  x="50%"
+                  y={-1}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <path d="M.5 200V.5H200" fill="none" />
+                </pattern>
+              </defs>
+              <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+                <path
+                  d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                  strokeWidth={0}
+                />
+              </svg>
+              <rect
+                width="100%"
+                height="100%"
                 strokeWidth={0}
+                fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
               />
             </svg>
-            <rect
-              width='100%'
-              height='100%'
-              strokeWidth={0}
-              fill='url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)'
-            />
-          </svg>
-          <div
-            className='absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48'
-            aria-hidden='true'
-          >
             <div
-              className='aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#C75922] to-[#2834FE] opacity-30'
-              style={{
-                clipPath:
-                  'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
-              }}
-            />
-          </div>
-          <div className='overflow-hidden'>
-            <div className='mx-auto max-w-7xl px-6 pb-32 pt-32 sm:pt-60 lg:px-8 lg:pt-32'>
-              <div className='mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center'>
-                <div className='w-full max-w-xl lg:shrink-0 xl:max-w-2xl'>
-                  <h2 className='text-4xl font-bold tracking-tight text-blue-500 sm:text-4xl'>
-                    Elevate Your Community Experience
-                  </h2>
-                  <p className='relative mt-6 text-lg leading-8 text-gray-700 sm:max-w-md lg:max-w-none rounde p-3'>
-                    Our events unite enthusiasts, forge connections, and
-                    celebrate the spirit of competition. Explore our offerings,
-                    connect with like-minded individuals, and be part of a
-                    vibrant sporting community that values every participant.
-                    Together, we elevate our community through sportsmanship.
-                  </p>
+              className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+              aria-hidden="true"
+            >
+              <div
+                className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#C75922] to-[#2834FE] opacity-30"
+                style={{
+                  clipPath:
+                    "polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)",
+                }}
+              />
+            </div>
+            <div className="overflow-hidden">
+              <div className="mx-auto max-w-7xl px-6 pb-32 pt-32 sm:pt-60 lg:px-8 lg:pt-32">
+                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                  <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+                    <h2 className="text-4xl font-bold tracking-tight text-blue-500 sm:text-4xl">
+                      Elevate Your Community Experience
+                    </h2>
+                    <p className="relative mt-6 text-lg leading-8 text-gray-700 sm:max-w-md lg:max-w-none rounde p-3">
+                      Our events unite enthusiasts, forge connections, and
+                      celebrate the spirit of competition. Explore our
+                      offerings, connect with like-minded individuals, and be
+                      part of a vibrant sporting community that values every
+                      participant. Together, we elevate our community through
+                      sportsmanship.
+                    </p>
 
-                  <div className='mt-10 flex items-center gap-x-6'>
-                    <a
-                      href='/signup'
-                      className='rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                    >
-                      Join Now
-                    </a>
+                    <div className="mt-10 flex items-center gap-x-6">
+                      <a
+                        href="/signup"
+                        className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Join Now
+                      </a>
 
-                    <a
-                      href='/events'
-                      className='rounded-md text-sm font-semibold leading-6 text-gray-900'
-                    >
-                      Explore Events
-                    </a>
-                  </div>
-                </div>
-                <div className='mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0'>
-                  <div className='ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80'>
-                    <div className='relative'>
-                      <img
-                        src='https://res.cloudinary.com/dtyw96fds/image/upload/v1695156455/small_physiquecomp_5b54da58be.jpg'
-                        alt=''
-                        className='aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg'
-                      />
-                      <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
+                      <a
+                        href="/events"
+                        className="rounded-md text-sm font-semibold leading-6 text-gray-900"
+                      >
+                        Explore Events
+                      </a>
                     </div>
                   </div>
-                  <div className='mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36'>
-                    <div className='relative'>
-                      <img
-                        src='https://res.cloudinary.com/dtyw96fds/image/upload/v1695156455/small_pageant2_5e5c687dd3.jpg'
-                        alt=''
-                        className='aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg'
-                      />
-                      <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
+                  <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                    <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                      <div className="relative">
+                        <img
+                          src="https://res.cloudinary.com/dtyw96fds/image/upload/v1695156455/small_physiquecomp_5b54da58be.jpg"
+                          alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        />
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                      </div>
                     </div>
-                    <div className='relative'>
-                      <img
-                        src='https://res.cloudinary.com/dtyw96fds/image/upload/v1694359186/small_event2_9b4f770b5b.jpg'
-                        alt=''
-                        className='aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg'
-                      />
-                      <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
+                    <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                      <div className="relative">
+                        <img
+                          src="https://res.cloudinary.com/dtyw96fds/image/upload/v1695156455/small_pageant2_5e5c687dd3.jpg"
+                          alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        />
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                      </div>
+                      <div className="relative">
+                        <img
+                          src="https://res.cloudinary.com/dtyw96fds/image/upload/v1694359186/small_event2_9b4f770b5b.jpg"
+                          alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        />
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                      </div>
                     </div>
-                  </div>
-                  <div className='w-44 flex-none space-y-8 pt-32 sm:pt-0'>
-                    <div className='relative'>
-                      <img
-                        src='https://res.cloudinary.com/dtyw96fds/image/upload/v1694355766/samples/two-ladies.jpg'
-                        alt=''
-                        className='aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg'
-                      />
-                      <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
-                    </div>
-                    <div className='relative'>
-                      <img
-                        src='https://res.cloudinary.com/dtyw96fds/image/upload/v1695156456/small_wrestling_cdae7afd34.jpg'
-                        alt=''
-                        className='aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg'
-                      />
-                      <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
+                    <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                      <div className="relative">
+                        <img
+                          src="https://res.cloudinary.com/dtyw96fds/image/upload/v1694355766/samples/two-ladies.jpg"
+                          alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        />
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                      </div>
+                      <div className="relative">
+                        <img
+                          src="https://res.cloudinary.com/dtyw96fds/image/upload/v1695156456/small_wrestling_cdae7afd34.jpg"
+                          alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        />
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
     </>
-  )
+  );
 }
