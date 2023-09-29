296f78c1186b601983db0f126a1937167d17cf47
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-09-29 02:13:52 -0400

    eslint fixes

--- app/components/mobileHero.js
+++ app/components/mobileHero.js
@@ -1,7 +1,3 @@
-
-
 export default function MobileHero() {
-  return (
-    <div className="h-screen w-screen bg-black"></div>
-  )
+  return <div className="h-screen w-screen bg-black"></div>;
 }
--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,14 +1,18 @@
 import localFont from "next/font/local";
 
-const oswald = localFont({src: '../styles/Oswald/Oswald-VariableFont_wght.ttf'})
-const openSans = localFont({ src: '../styles/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf'})
+const oswald = localFont({
+  src: "../styles/Oswald/Oswald-VariableFont_wght.ttf",
+});
+const openSans = localFont({
+  src: "../styles/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf",
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className= {`${openSans.className}`}>
+    <main className={`${openSans.className}`}>
       <Component {...pageProps} />
     </main>
   );
 }
 
-export {  oswald, openSans };
+export { oswald, openSans };
