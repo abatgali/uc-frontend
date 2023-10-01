0b33406a3395b82f0b9e4b763397c68825315a18
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-09-30 22:33:27 -0400

    eslint fix

--- app/components/live_platform_logos.js
+++ app/components/live_platform_logos.js
@@ -2,25 +2,47 @@ import brandicon from "../youtube.svg";
 import Image from "next/image";
 import facebook from "../Facebook_Logo_Primary.png";
 import rumble from "../rumble.svg";
-import insta from '../Instagram_Glyph_Gradient.svg'
-import tiktok from '../TikTok_Icon_Black_Circle.png'
+import insta from "../Instagram_Glyph_Gradient.svg";
+import tiktok from "../TikTok_Icon_Black_Circle.png";
 
 export function YouTube() {
-  return <Image src={brandicon} className="w-auto h-16 max-sm:h-10" alt="youtube logo" />;
+  return (
+    <Image
+      src={brandicon}
+      className="w-auto h-16 max-sm:h-10"
+      alt="youtube logo"
+    />
+  );
 }
 
 export function Facebook() {
-  return <Image src={facebook} className="w-auto h-16 max-sm:h-10 max-sm:w-auto" alt="facebook logo" />;
+  return (
+    <Image
+      src={facebook}
+      className="w-auto h-16 max-sm:h-10 max-sm:w-auto"
+      alt="facebook logo"
+    />
+  );
 }
 
 export function Rumble() {
-  return <Image src={rumble} className="w-auto h-16 max-sm:h-10" alt="Rumble logo" />;
+  return (
+    <Image src={rumble} className="w-auto h-16 max-sm:h-10" alt="Rumble logo" />
+  );
 }
 
 export function Instagram() {
-  return <Image src={insta} className="w-auto h-16 max-sm:h-10" alt="Instagram logo" />;
+  return (
+    <Image
+      src={insta}
+      className="w-auto h-16 max-sm:h-10"
+      alt="Instagram logo"
+    />
+  );
 }
 
 export function TikTok() {
-  return <Image src={tiktok} className="w-auto h-16 max-sm:h-10" alt="TikTok logo" />;
+  return (
+    <Image src={tiktok} className="w-auto h-16 max-sm:h-10" alt="TikTok logo" />
+  );
 }
