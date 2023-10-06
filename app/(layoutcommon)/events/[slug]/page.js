79926f57b1501d5847376d7bf298c37de8322298
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-10-06 09:37:52 -0400

    slug event page

--- app/(layoutcommon)/events/[slug]/page.js
+++ app/(layoutcommon)/events/[slug]/page.js
@@ -1,9 +1,5 @@
 export default function EventDetails() {
-
   return (
-    <div className="h-screen bg-gradient-to-br from-black to-slate-400 text-white w-screen">
-        
-    </div>
-    
-  )
+    <div className="h-screen bg-gradient-to-br from-black to-slate-400 text-white w-screen"></div>
+  );
 }
