7a559f3d72c28be66e33208b142c67082bf47131
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-10-18 00:51:11 -0400

    before setting up auth checkpoint

--- app/(layoutcommon)/boardmember/page.js
+++ app/(layoutcommon)/boardmember/page.js
@@ -1,15 +1,13 @@
 "use client";
 import Link from "next/link";
 import { oswald } from "@/app/pages/_app";
-import {  useState } from "react";
+import { useState } from "react";
 
 import BoardLogin from "@/app/components/boardportal";
 
 export default function BoardPortal() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-
   return (
     <div className="h-screen w-full bg-gradient-to-b from-gray-950 to-blue-300 ">
       <BoardLogin />
