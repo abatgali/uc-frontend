a44d160b003e6e9cecab0616d48b550c64d79a71
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-11-04 01:40:34 -0400

    Remove BoardPortal component from BoardMember page.

--- app/(layoutcommon)/boardmember/page.js
+++
@@ -1,8 +0,0 @@
-import BoardLogin from "@/app/components/boardportal";
-export default function BoardPortal() {
-  return (
-    <div className="h-screen w-full bg-gradient-to-b from-gray-950 to-blue-300 ">
-      <BoardLogin />
-    </div>
-  );
-}
