ae79c51958d2a3d57189f24dab1c6689c0fe149e
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-11-04 02:24:26 -0400

    Refactor SignIn component to remove unused props

--- app/(authorize)/login/page.js
+++ app/(authorize)/login/page.js
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default function SignIn({ supabaseClient, initialSession }) {
+export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
