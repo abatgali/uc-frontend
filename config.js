b901ba3a777db9a7e98d4cc403d48e67c5997a27
Anant Batgali <anantbatgali@Anants-MacBook-Air.local>
2023-11-25 04:40:28 -0500

    Update appDescription and mailgun settings

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
   appName: "UMAA - United Myanmar American Association",
   // REQUIRED: a short description of your app for SEO tags (can be overwritten)
   appDescription:
-    "The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app.",
+    "UMAA - United Myanmar American Association is a non-profit organization that aims to promote the welfare of the Myanmar American community in the United States.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
   domainName: "unitedmyanmar.org",
   crisp: {
@@ -72,15 +72,15 @@ const config = {
   },
   mailgun: {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
-    subdomain: "mg",
+    subdomain: "mail",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `ShipFast <noreply@mg.shipfa.st>`,
+    fromNoReply: `UMAA <noreply@mail.unitedmyanmar.org>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `Marc at ShipFast <marc@mg.shipfa.st>`,
+    fromAdmin: `Admin at UMAA <unitedchin.uci@gmail.com>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
-    supportEmail: "marc@mg.shipfa.st",
+    supportEmail: "unitedchin.uci@gmail.com",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
-    forwardRepliesTo: "marc.louvion@gmail.com",
+    forwardRepliesTo: "unitedchin.uci@gmail.com",
   },
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
