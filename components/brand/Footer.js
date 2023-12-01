fa8c876ff3567d13c558599dc25003b21a788ee5
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-12-01 04:21:23 -0500

    Add link to user profile and update member portal name

--- components/brand/Footer.js
+++ components/brand/Footer.js
@@ -1,8 +1,5 @@
-"use client";
 import Link from "next/link";
-// import { oswald } from "../pages/_app";
 import Image from "next/image";
-import ButtonAccount from "../ButtonAccount";
 import ButtonSignin from "../ButtonSignin";
 
 const navigation = {
@@ -93,6 +90,7 @@ const navigation = {
           height="1.8em"
           viewBox="0 0 512 512"
           fill="rgb(156, 163, 175)"
+          {...props}
         >
           <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
         </svg>
@@ -140,7 +138,7 @@ export default function Footer() {
             <div
               className={` text-amber-700 text-lg hover:text-white flex items-center gap-3 max-sm:justify-around`}
             >
-              <Link href="/signin">Board Member Portal</Link>
+              <Link href="/signin">Member Portal</Link>
               <div className="">
                 <ButtonSignin />
               </div>
--- components/brand/MembersList.js
+++ components/brand/MembersList.js
@@ -1,6 +1,33 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import toast from "react-hot-toast";
 
 export default function MembersList() {
+  const supabase = createClientComponentClient();
+  const [members, setMembers] = useState([]);
+  const fetchMembers = async () => {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("username, full_name");
+
+    if (error) {
+      console.log(error);
+      toast.error(error.message);
+    } else {
+      setMembers(data);
+      console.log(members);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchMembers();
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <details className="collapse bg-base-200">
@@ -8,7 +35,42 @@ export default function MembersList() {
           Members
         </summary>
         <div className="collapse-content">
-          <p>content</p>
+          <div className="overflow-x-auto">
+            <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm text-center">
+              <thead className="">
+                <tr>
+                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Name
+                  </th>
+                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Username
+                  </th>
+                  <th className="px-4 py-2"></th>
+                </tr>
+              </thead>
+
+              <tbody className="divide-y divide-gray-200">
+                {members.map((member) => (
+                  <tr key={member.username}>
+                    <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                      {member.full_name}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      {member.username}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2">
+                      <a
+                        href="#"
+                        className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+                      >
+                        View
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </details>
     </>
